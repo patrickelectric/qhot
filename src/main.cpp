a261aadddb4419b6b9e7b220d8a4d3e18606e632
Jeroen Oomkes <jeruntu@gmail.com>
2021-01-11 16:34:15 +0100

    src: Fix clazy warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include <QGuiApplication>
-#include <QDebug>
-#include <QDir>
-#include <QFile>
-#include <QQmlApplicationEngine>
-
 #include "commandlineparser.h"
 #include "providessomething.h"
 #include "urlinterceptor.h"
+
+#include <QtCore/QDebug>
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
@@ -25,13 +25,14 @@ int main(int argc, char *argv[])
     appEngine.setUrlInterceptor(UrlInterceptor::self());
     ProvidesSomething::self()->setEngine(&appEngine);
 
-    if(commandLineParser.positionalArguments().size()) {
-        QDir dir(commandLineParser.positionalArguments()[0]);
-        if(!QFile::exists(dir.absolutePath())) {
-            qDebug() << "Path" << dir.absolutePath() << "does not exist";
+    if (commandLineParser.positionalArguments().size()) {
+        QDir dir(commandLineParser.positionalArguments().at(0));
+        if (!QFile::exists(dir.absolutePath())) {
+            qWarning() << "Path" << dir.absolutePath() << "does not exist";
         } else {
             ProvidesSomething::self()->setFilePath(QUrl::fromUserInput(dir.absolutePath()));
         }
     }
+
     return app.exec();
 }
